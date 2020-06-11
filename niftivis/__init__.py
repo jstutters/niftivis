6091c4573c12571719e61369ea4f0d0eeb769ec6
Jon Stutters <jstutters@jeremah.co.uk>
2020-06-11 15:51:04 +0100

    Add first test

--- niftivis/__init__.py
+++ niftivis/__init__.py
@@ -1,4 +1,5 @@
 from niftivis.niftivis import make_thumbnails
 
 
+__version__ = "202006.1"
 __all__ = ["make_thumbnails"]
---
+++ tests/test_niftivis.py
@@ -0,0 +1,5 @@
+from niftivis import __version__
+
+
+def test_version():
+    assert __version__ == '202006.1'
